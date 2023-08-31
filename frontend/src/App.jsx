1f0a566ed28e39f33ad1d3877a227914cc330453
mbeka02 <antonymbeka@gmail.com>
2023-08-31 17:14:50 +0300

    updated styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import Chat from "./components/Chat";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="font-varela">
+    <div className="font-varela relative">
+      <Navbar />
       <Chat />
     </div>
   );
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+const Navbar = () => {
+  return (
+    <div className="topnav" style={{ maxHeight: "100px " }}>
+      <div className="navlogo flex items-center">
+        <img
+          className="tttlogo"
+          src="/cross.png"
+          alt="logo"
+          style={{
+            height: "60px",
+            paddingRight: "6px",
+            verticalAlign: "middle",
+          }}
+        />
+        <div className=" relative inline-block">
+          <a
+            href="https://github.com/romannjoroge/A2SVTeamDataSpies"
+            target="_blank"
+            className="underline_on_hover"
+          >
+            MediGuide
+          </a>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
